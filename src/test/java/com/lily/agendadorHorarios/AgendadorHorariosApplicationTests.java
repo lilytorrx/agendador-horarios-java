5ce3950fa5e118437cf7942019f73d1d997e060c
lilytorrx <109477475+lilytorrx@users.noreply.github.com>
2026-03-04 12:48:32 -0300

    feat: Estrutura inicial do projeto (Spring Initializr)

---
+++ src/test/java/com/lily/agendadorHorarios/AgendadorHorariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lily.agendadorHorarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorHorariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
