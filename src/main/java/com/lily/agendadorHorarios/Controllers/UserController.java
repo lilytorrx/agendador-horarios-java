d752e899624c67241722f1a33a1054f9fb3f03ea
lilytorrx <109477475+lilytorrx@users.noreply.github.com>
2026-03-06 19:32:30 -0300

    feat: done UserController for getUser in API

--- src/main/java/com/lily/agendadorHorarios/Controllers/UserController.java
+++ src/main/java/com/lily/agendadorHorarios/Controllers/UserController.java
@@ -1,4 +1,16 @@
 package com.lily.agendadorHorarios.Controllers;
 
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    @GetMapping
+    public ResponseEntity<String> getUser() {
+        return ResponseEntity.ok("Sucesso!");
+    }
 }
